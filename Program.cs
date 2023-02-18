fed78c683fabfe0f61079886f782b203df4eeb85
Gasan <1793959@mail.rum>
2023-02-18 22:15:49 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Проверка без применения строк
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+using System;
+
+class Palindr
+     {
+        public static bool Palindromtest (string s)
+        {
+               for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+ 
+                    if (s[i] == s[j]) 
+                        return true; 
+                        return false;                   
+        }
+          
+
+    static void Main()
+        {
+        string s;
+            Console.WriteLine("Анализ палиндромов \n\n Введите строку:");
+          s = Console.ReadLine();
+           if (Palindromtest(s))
+       Console.WriteLine("Эта строка - палиндром");
+       else Console.WriteLine("Эта строка - не палиндром");
+    Console.ReadLine();
+        }
+}
